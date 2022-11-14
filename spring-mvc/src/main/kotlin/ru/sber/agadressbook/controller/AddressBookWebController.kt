893f7d3b314134479b4a47e440ae1123cf2cd82e
Gaponchuk Alexey <aggaponchuk@gmail.com>
2022-11-14 17:37:14 +0300

    work version

--- spring-mvc/src/main/kotlin/ru/sber/agadressbook/controller/AddressBookWebController.kt
+++ spring-mvc/src/main/kotlin/ru/sber/agadressbook/controller/AddressBookWebController.kt
@@ -2,6 +2,8 @@ package ru.sber.agadressbook.controller
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import ru.sber.agadressbook.models.Person
@@ -10,12 +12,15 @@ import ru.sber.agadressbook.service.AddressBookWebService
 @Controller
 class AddressBookWebController @Autowired constructor(val addressBookWebService: AddressBookWebService) {
 
-
-
     @GetMapping("/person/{id}")
-    fun getPerson(@PathVariable("id") id : Int) {
+    fun getPerson(@PathVariable("id") id : Int, model : Model) : String {
         println("We are in controller $id")
-        addressBookWebService.getPersonById(id)
+        println("PERSON ${addressBookWebService.getPersonById(id)}")
+        addressBookWebService.getPersonById(id)?.firstName?.let { model["firstName"] = it }
+        model["firstName"] = addressBookWebService.getPersonById(id)?.firstName!!
+        //model["firstName"] = "TEST TEST TEST"
+        model["title"] = "GGGGGGGGGGGGGGGGGGGG!!!!!!!!!!!!!"
+        return "test"
     }
 
 }
