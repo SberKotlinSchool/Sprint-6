eca281d7edc59ec7caf577465ab7e632b2d8a4be
Goshiq <vangog-2005@ngs.ru>
2022-11-17 22:38:02 +0300

    Sprint5: clean up

--- webserver/src/main/kotlin/FileServer.kt
+++ webserver/src/main/kotlin/FileServer.kt
@@ -31,46 +31,13 @@ class FileServer {
          */
         while (true) {
 
-            // TODO Delete this once you start working on your solution.
-            //throw new UnsupportedOperationException();
-
-            // TODO 1) Use socket.accept to get a Socket object
             val socket = serverSocket.accept()
 
-            /*
-            * TODO 2) Using Socket.getInputStream(), parse the received HTTP
-            * packet. In particular, we are interested in confirming this
-            * message is a GET and parsing out the path to the file we are
-            * GETing. Recall that for GET HTTP packets, the first line of the
-            * received packet will look something like:
-            *
-            *     GET /path/to/file HTTP/1.1
-            */
             val inputStream = socket.getInputStream()
             val bufferedReader = BufferedReader(InputStreamReader(inputStream))
 
             val filePath = bufferedReader.readLine().split(" ")[1]
 
-            /*
-             * TODO 3) Using the parsed path to the target file, construct an
-             * HTTP reply and write it to Socket.getOutputStream(). If the file
-             * exists, the HTTP reply should be formatted as follows:
-             *
-             *   HTTP/1.0 200 OK\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *   FILE CONTENTS HERE\r\n
-             *
-             * If the specified file does not exist, you should return a reply
-             * with an error code 404 Not Found. This reply should be formatted
-             * as:
-             *
-             *   HTTP/1.0 404 Not Found\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *
-             * Don't forget to close the output stream.
-             */
             val outputStream = socket.getOutputStream()
             val file = fs.readFile(VPath(filePath))
             var responseText = "HTTP/1.0 404 Not Found\nServer: FileServer\n\n"
