ab7750647ebccf33bab294ab307c8350f9fa44fe
Gaponchuk Alexey <aggaponchuk@gmail.com>
2022-11-17 16:36:05 +0300

    add html forms

--- spring-mvc/src/main/kotlin/ru/sber/agadressbook/controller/AddressBookWebController.kt
+++ spring-mvc/src/main/kotlin/ru/sber/agadressbook/controller/AddressBookWebController.kt
@@ -6,9 +6,11 @@ import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 import ru.sber.agadressbook.models.Person
 import ru.sber.agadressbook.service.AddressBookWebService
 
+@RequestMapping("/address-book")
 @Controller
 class AddressBookWebController @Autowired constructor(val addressBookWebService: AddressBookWebService) {
 
@@ -19,5 +21,37 @@ class AddressBookWebController @Autowired constructor(val addressBookWebService:
         model["title"] = "This is a address-book title"
         return "test"
     }
+
+    @GetMapping("/")
+    fun index() : String {
+        return "redirect:/address-book/login_form"
+    }
+
+    @GetMapping("login_form")
+    fun login() : String {
+        return "login_form"
+    }
+
+    @GetMapping("add_record")
+    fun addRecord() : String {
+        return "add_record"
+    }
+
+    @GetMapping("edit_record")
+    fun editRecord() : String {
+        return "edit_record"
+    }
+
+    @GetMapping("delete_record")
+    fun deleteRecord() : String {
+        return "delete_record"
+    }
+
+    @GetMapping("all_records")
+    fun getAllRecord() : String {
+        return "all_records"
+    }
+
+
 
 }
