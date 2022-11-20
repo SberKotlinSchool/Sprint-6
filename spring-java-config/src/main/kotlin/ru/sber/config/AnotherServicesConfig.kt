2edbdd4c1b6db0a101d6d8f5651ab959ef9a3f17
Korshik <dakorshik@sberbank.ru>
2022-11-20 18:30:51 +0300

    Solve spring-java-config

--- spring-java-config/src/main/kotlin/ru/sber/config/AnotherServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/AnotherServicesConfig.kt
@@ -4,5 +4,5 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan("ru.sber.anotherservices")
+@ComponentScan("ru.sber.services")
 class AnotherServicesConfig
