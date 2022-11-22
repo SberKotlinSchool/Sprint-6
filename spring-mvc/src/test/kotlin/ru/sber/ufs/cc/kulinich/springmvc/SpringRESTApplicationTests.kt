3ddb28ed7a2cb248e4e4fe17d06bbf9ef325fc83
roman kulinich <kulinichrs@gmail.com>
2022-11-22 13:55:51 +0300

    rest fix

--- spring-mvc/src/test/kotlin/ru/sber/ufs/cc/kulinich/springmvc/SpringRESTApplicationTests.kt
+++ spring-mvc/src/test/kotlin/ru/sber/ufs/cc/kulinich/springmvc/SpringRESTApplicationTests.kt
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.http.HttpEntity
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJson
-import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.test.web.server.LocalServerPort
