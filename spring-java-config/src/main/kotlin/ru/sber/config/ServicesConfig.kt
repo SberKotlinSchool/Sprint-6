49c5e6f75f00829136f7242cad7ff9131f1000c7
albertincx <albertincx@gmail.com>
2022-11-24 22:24:26 +0300

    spring-1

--- spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -1,19 +1,8 @@
 package ru.sber.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import ru.sber.services.FirstService
 
 @Configuration
-@ComponentScan
-class ServicesConfig {
-    @Bean
-    fun service(): FirstService {
-        return FirstService()
-    }
-
-    @Bean
-    fun secondService() {
-    }
-}
+@ComponentScan("ru.sber.services")
+class ServicesConfig
