377b59338fb6f18314026a734c4bb5318a529d11
Goshiq <vangog-2005@ngs.ru>
2022-11-25 15:32:46 +0300

    Sprint5: mvc added mockMvc testing post-method (register)

--- spring-mvc/src/test/kotlin/ru/sber/springmvc/SpringMvcApplicationTests.kt
+++ spring-mvc/src/test/kotlin/ru/sber/springmvc/SpringMvcApplicationTests.kt
@@ -1,13 +1,15 @@
 package ru.sber.springmvc
 
+import org.hamcrest.core.StringContains.containsString
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import org.springframework.util.LinkedMultiValueMap
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -20,6 +22,7 @@ class SpringMvcApplicationTests {
 	fun testIndex() {
 		mockMvc.perform(get("/"))
 			.andExpect(status().isOk)
+			.andExpect(content().string(containsString("What do you want?")))
 	}
 
 	@Test
@@ -27,6 +30,18 @@ class SpringMvcApplicationTests {
 		mockMvc.perform(get("/api/login?name=user&pass=user"))
 			.andExpect(status().isOk)
 			.andExpect(jsonPath("$.status").value("fail"))
+	}
+
+	@Test
+	fun testRegister() {
+		val map = LinkedMultiValueMap<String, String>()
+		map.add("userName", "test")
+		map.add("password", "test")
+
+		mockMvc.perform(post("/register").params(map))
+			.andExpect(status().isOk)
+			.andExpect(view().name("signIn"))
+			.andExpect((content().string(containsString("Now enter your new credentials"))))
 	}
 
 }
