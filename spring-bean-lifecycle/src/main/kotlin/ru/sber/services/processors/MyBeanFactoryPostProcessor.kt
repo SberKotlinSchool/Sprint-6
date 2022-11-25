7cab93dc87c59b4c5c84945bb1c0edd96491ab93
MorningCake <kireevds7@gmail.com>
2022-11-25 19:59:19 +0400

    SPRINT-6 - жизненный цикл бинов

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
@@ -7,5 +7,18 @@ import org.springframework.stereotype.Component
 @Component
 class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor {
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
+//        val bean = beanFactory.getBean("beanFactoryPostProcessorBean")
+//        val clazz = bean.javaClass
+//        val postProcessorInterface =
+//            clazz.interfaces.first { it.name == "ru.sber.services.BeanFactoryPostProcessorInterface" }
+//        val postConstructMethod = postProcessorInterface.methods.first {
+//            it.isAnnotationPresent(PostConstruct::class.java)
+//        }
+//        val name = postConstructMethod.name.toString()
+//        if (name == "postConstruct") { println("Равно") }
+//        val beanDefinition = beanFactory.getBeanDefinition("beanFactoryPostProcessorBean")
+//        beanDefinition.initMethodName = name.trim()
+//        if (beanDefinition.initMethodName == "postConstruct") { println("Равно") }
+        beanFactory.getBeanDefinition("beanFactoryPostProcessorBean").initMethodName = "postConstruct"
     }
 }
