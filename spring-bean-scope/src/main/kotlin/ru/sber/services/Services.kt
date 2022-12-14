93d2a19acd2c1cbf5bc833d662591aa0e6579138
Anna Popova <anna.popova201909@gmail.com>
2022-12-14 18:44:28 +0300

    ready for PR

---
+++ spring-bean-scope/src/main/kotlin/ru/sber/services/Services.kt
@@ -0,0 +1,20 @@
+package ru.sber.services
+
+import org.springframework.context.annotation.Scope
+import org.springframework.stereotype.Service
+
+@Service("singletonService")
+@Scope("singleton")
+class FirstService {
+    override fun toString(): String {
+        return "I am firstService"
+    }
+}
+
+@Service("prototypeService")
+@Scope("prototype")
+class SecondService {
+    override fun toString(): String {
+        return "I am secondService"
+    }
+}
