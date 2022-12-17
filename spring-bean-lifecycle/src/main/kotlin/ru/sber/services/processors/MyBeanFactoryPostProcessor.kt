fb58d7e8ed12b433a25878a2a544f730c28b07de
Dmitry Biryukov <dmitrybiryukov4@gmail.com>
2022-12-18 02:03:32 +0300

    WebServer, Spring_FW_1 and Spring_FW_2 are done

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
@@ -3,9 +3,20 @@ package ru.sber.services.processors
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.stereotype.Component
+import ru.sber.services.BeanFactoryPostProcessorBean
+import javax.annotation.PostConstruct
 
 @Component
 class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor {
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
+        BeanFactoryPostProcessorBean::class.java.interfaces.forEach { interFace->
+            interFace.methods.forEach { interfaceMethod ->
+                interfaceMethod.annotations.filterIsInstance<PostConstruct>().forEach { _ ->
+                    when (interfaceMethod.name){
+                        "postConstruct" -> beanFactory.getBean(BeanFactoryPostProcessorBean::class.java).postConstruct()
+                    }
+                }
+            }
+        }
     }
 }
