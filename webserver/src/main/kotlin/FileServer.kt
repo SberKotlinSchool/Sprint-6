1399a92dbcebc745e4e76ed3e57bdfddb14db822
DudnikSlava <slavadvu@gmail.com>
2022-12-19 22:17:12 +0300

    Спринт 6. Webserver

--- webserver/src/main/kotlin/FileServer.kt
+++ webserver/src/main/kotlin/FileServer.kt
@@ -1,6 +1,9 @@
 import ru.sber.filesystem.VFilesystem
+import ru.sber.filesystem.VPath
 import java.io.IOException
+import java.io.PrintWriter
 import java.net.ServerSocket
+import java.net.Socket
 
 /**
  * A basic and very limited implementation of a file server that responds to GET
@@ -28,43 +31,28 @@ class FileServer {
          */
         while (true) {
 
-            // TODO Delete this once you start working on your solution.
-            //throw new UnsupportedOperationException();
+            socket.accept().use { serverSocket ->
+                serverSocket.getInputStream().bufferedReader().use { reader ->
+                    val filePath = reader.readLine().split(" ")[1]
+                    val readFile = fs.readFile(VPath(filePath))
+                    PrintWriter(serverSocket.getOutputStream()).use { writer ->
+                        when {
+                            readFile != null ->
+                                writer
+                                    .append("HTTP/1.0 200 OK")
+                                    .appendLine("Server: FileServer")
+                                    .appendLine("")
+                                    .appendLine(readFile)
+                                    .appendLine("")
+                            else -> writer
+                                .append("HTTP/1.0 404 Not Found")
+                                .appendLine("Server: FileServer")
+                                .appendLine("")
+                        }
+                    }
 
-            // TODO 1) Use socket.accept to get a Socket object
-
-
-            /*
-            * TODO 2) Using Socket.getInputStream(), parse the received HTTP
-            * packet. In particular, we are interested in confirming this
-            * message is a GET and parsing out the path to the file we are
-            * GETing. Recall that for GET HTTP packets, the first line of the
-            * received packet will look something like:
-            *
-            *     GET /path/to/file HTTP/1.1
-            */
-
-
-            /*
-             * TODO 3) Using the parsed path to the target file, construct an
-             * HTTP reply and write it to Socket.getOutputStream(). If the file
-             * exists, the HTTP reply should be formatted as follows:
-             *
-             *   HTTP/1.0 200 OK\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *   FILE CONTENTS HERE\r\n
-             *
-             * If the specified file does not exist, you should return a reply
-             * with an error code 404 Not Found. This reply should be formatted
-             * as:
-             *
-             *   HTTP/1.0 404 Not Found\r\n
-             *   Server: FileServer\r\n
-             *   \r\n
-             *
-             * Don't forget to close the output stream.
-             */
+                }
+            }
         }
     }
 }
