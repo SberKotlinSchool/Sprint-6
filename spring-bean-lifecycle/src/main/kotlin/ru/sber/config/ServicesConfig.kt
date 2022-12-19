fca8eda28843abb6ca065bb498f7982073640ca4
DudnikSlava <slavadvu@gmail.com>
2022-12-19 23:55:17 +0300

    Spring 6. Lifecycle

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import ru.sber.services.CombinedBean
+import ru.sber.services.processors.MyBeanFactoryPostProcessor
 
 @Configuration
 @ComponentScan("ru.sber.services")
 class ServicesConfig {
-    @Bean
+    @Bean(initMethod = "customInit")
     fun combinedBean() = CombinedBean()
 }
--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
@@ -3,9 +3,33 @@ package ru.sber.services.processors
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
 import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
 
 @Component
 class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor {
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
+
+        Class.forName("ru.sber.services.BeanFactoryPostProcessorInterface").methods.singleOrNull { method ->
+            method.isAnnotationPresent(PostConstruct::class.java)
+        }?.let { initMethod ->
+            beanFactory.beanDefinitionNames.forEach { bdn ->
+                beanFactory.getBeanDefinition(bdn)?.let { bd ->
+                    bd.beanClassName?.let {beanClassName ->
+                        Class.forName(beanClassName).also { clazz ->
+                            clazz.genericInterfaces.singleOrNull { it.typeName == "ru.sber.services.BeanFactoryPostProcessorInterface" }
+                                ?.let {
+                                    bd.initMethodName = initMethod.name
+                                }
+
+
+                        }
+                    }
+
+                }
+
+            }
+        }
+
+
     }
 }
