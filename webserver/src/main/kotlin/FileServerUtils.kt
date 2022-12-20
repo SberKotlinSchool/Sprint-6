0e22ea65876bff20f4673a83cc99620bc1765da3
Воротов Алексей Викторович <Vorotov.A.Vikt@sberbank.ru>
2022-12-20 07:55:28 +0300

    Sprint 6. DI

--- webserver/src/main/kotlin/FileServerUtils.kt
+++
@@ -1,6 +0,0 @@
-import java.net.http.HttpRequest
-
-fun getHttpRequest(httpRequestBody: String): HttpRequest {
-    val (method, path, version) = httpRequestBody.split(" ")
-    return HttpRequest.newBuilder().GET().build()
-}
--- webserver/src/test/java/FileServerUtilsTest.kt
+++
@@ -1,8 +0,0 @@
-
-class FileServerUtilsTest {
-
-    fun goodBodyTest() {
-
-    }
-
-}
