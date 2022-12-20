318cb2dc3b3635c319a6ca3272e23b776e376282
Zaynulina-GT <Zaynulina.G.T@sberbank.ru>
2022-12-20 22:03:25 +0500

    Sprint-5 Spring DI

--- spring-dependency-injection/src/main/kotlin/ru/sber/services/Services.kt
+++ spring-dependency-injection/src/main/kotlin/ru/sber/services/Services.kt
@@ -1,9 +1,11 @@
 package ru.sber.services
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Primary
 import org.springframework.stereotype.Component
 
 @Component
-class ConstructorInjectionService(private val service: Service) {
+class ConstructorInjectionService @Autowired constructor(private val service: Service) {
 
     constructor(service: Service, someField: Int) : this(AnotherService())
 
@@ -14,7 +16,7 @@ class ConstructorInjectionService(private val service: Service) {
 
 @Component
 class FieldInjectionService {
-    private lateinit var service: Service
+    @Autowired private lateinit var service: Service
 
     override fun toString(): String {
         return "$service was injected into FieldInjectionService"
@@ -24,6 +26,7 @@ class FieldInjectionService {
 @Component
 class SetterInjectionService {
     lateinit var service: Service
+    @Autowired set
 
     override fun toString(): String {
         return "$service was injected into SetterInjectionService"
@@ -31,6 +34,7 @@ class SetterInjectionService {
 }
 
 @Component
+@Primary
 class Service {
     override fun toString(): String {
         return "Service"
