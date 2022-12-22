2a4d3fe181134764bd326b1dc96eb179062de355
Tatiana Dolgopolova <tdolgopol@mail.ru>
2022-12-23 00:37:52 +0600

    spring-bean-lifecycle

--- spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
+++ spring-bean-lifecycle/src/main/kotlin/ru/sber/services/processors/MyBeanFactoryPostProcessor.kt
@@ -2,10 +2,21 @@ package ru.sber.services.processors
 
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+import org.springframework.beans.factory.getBeansOfType
 import org.springframework.stereotype.Component
+import ru.sber.services.BeanFactoryPostProcessorInterface
+import javax.annotation.PostConstruct
 
 @Component
 class MyBeanFactoryPostProcessor : BeanFactoryPostProcessor {
     override fun postProcessBeanFactory(beanFactory: ConfigurableListableBeanFactory) {
+        beanFactory.beanDefinitionNames.map {name -> beanFactory.getBeanDefinition(name) }
+             .forEach { beanDef ->
+                 beanDef.beanClassName?.let { clzName ->
+                     Class.forName(clzName).interfaces.flatMap { inf -> inf.methods.asIterable() }
+                         .firstOrNull { method -> method.isAnnotationPresent(PostConstruct::class.java) }
+                         ?.let { beanDef.initMethodName = it.name }
+                 }
+             }
     }
 }
