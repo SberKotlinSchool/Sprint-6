144f9cdc5d81ce9201fdd8fd68ba5b185eb7b444
SlumdogProgrammer <bolotin99@inbox.ru>
2023-01-28 23:29:29 +0300

    Spring Framework III - done

--- spring-dependency-injection/src/main/kotlin/ru/sber/services/QualifierServices.kt
+++ spring-dependency-injection/src/main/kotlin/ru/sber/services/QualifierServices.kt
@@ -13,7 +13,7 @@ class FirstQualifierServiceImpl : QualifierInterface {
     }
 }
 
-@Component
+@Component("onlySecondService")
 class SecondQualifierServiceImpl : QualifierInterface {
     override fun toString(): String {
         return "SecondQualifierServiceImpl"
