19d9e7eb3c1d7cad5fb6ccddf77ed8207e4cf6f1
Красненков Роман Сергеевич <RSKrasnenkov@sberbank.ru>
2023-11-21 20:16:56 +0300

    Добавил логику по определению бинов в конфигурации для прохождения тестов.

--- spring-java-config/src/main/kotlin/ru/sber/config/AnotherServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/AnotherServicesConfig.kt
@@ -1,8 +1,22 @@
 package ru.sber.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 
 @Configuration
 @ComponentScan("ru.sber.anotherservices")
-class AnotherServicesConfig
+class AnotherServicesConfig {
+
+    @Primary
+    @Bean
+    fun fourthService(): Any {
+        return object {
+            override fun toString(): String {
+                return "I am fourthService"
+            }
+        }
+    }
+
+}
