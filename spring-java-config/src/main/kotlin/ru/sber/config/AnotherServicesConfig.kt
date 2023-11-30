be52191366c95770f0394d437437882bf83542ea
Красненков Роман Сергеевич <RSKrasnenkov@sberbank.ru>
2023-11-30 19:11:54 +0300

    Изменил логику определения бина "fourthService" через изменения параметра сканирования пакетов для контекста.

--- spring-java-config/src/main/kotlin/ru/sber/config/AnotherServicesConfig.kt
+++ spring-java-config/src/main/kotlin/ru/sber/config/AnotherServicesConfig.kt
@@ -1,22 +1,8 @@
 package ru.sber.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 
 @Configuration
-@ComponentScan("ru.sber.anotherservices")
-class AnotherServicesConfig {
-
-    @Primary
-    @Bean
-    fun fourthService(): Any {
-        return object {
-            override fun toString(): String {
-                return "I am fourthService"
-            }
-        }
-    }
-
-}
+@ComponentScan("ru.sber.services")
+class AnotherServicesConfig
