ceed52b94d08df27fe0e5cd923766ecfa6bd0d3e
Yury Gorbunov <YVGorbunov@sberbank.ru>
2023-12-03 17:31:00 +0300

    Sprint-6. Homework. Всё кроме MVC.

--- spring-bean-scope/src/main/kotlin/ru/sber/config/ServicesConfig.kt
+++ spring-bean-scope/src/main/kotlin/ru/sber/config/ServicesConfig.kt
@@ -4,5 +4,5 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ComponentScan("ru.sber.services")
+@ComponentScan("ru.sber.config.services")
 class ServicesConfig
