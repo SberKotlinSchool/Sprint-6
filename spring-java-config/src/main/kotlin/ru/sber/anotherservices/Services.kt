160ea76e8e935634c4a3c0a6b2315d6d7c0e7b54
Yury Gorbunov <YVGorbunov@sberbank.ru>
2023-12-07 23:19:49 +0300

    Sprint-6. Поправил Spring-bean-lifecycle

--- spring-java-config/src/main/kotlin/ru/sber/anotherservices/Services.kt
+++ spring-java-config/src/main/kotlin/ru/sber/anotherservices/Services.kt
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service
 @Service
 class FourthService {
     override fun toString(): String {
-        return "I am fourthService"
+        return "I am anotherFourthService"
     }
 }
